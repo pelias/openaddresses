cf3b3b6e28634b2093b1834c31dc7f543e79ebb6
Severyn Kozak <severyn.kozak@gmail.com>
2015-01-15 14:01:27 -0500

    Add unit test for createRecordStream().
    test/(test.js, openaddresses_sample.csv)
    	-Add a thorough test for `createRecordStream()`, to check
    	whether all records are being read as expected.

--- test/test.js
+++ test/test.js
@@ -5,7 +5,35 @@
 'use strict';
 
 var tape = require( 'tape' );
+var through = require( 'through2' );
+var importPipelines = require( '../lib/import_pipelines' );
 
-tape( 'TODO: add unit tests.', function ( test ){
-  test.end();
+tape( 'importPipelines.createRecordStream().', function ( test ){
+  function createTestRec( lon, lat, name ){
+    return { lon: lon, lat: lat, name: name };
+  }
+
+  var expectedRecords = [
+    createTestRec( -118.0170157, 55.546026835788886, "23042 Twp Road 755 A" ),
+    createTestRec( -118.75318353, 55.14959214890181, "712046 Rge Road 34" ),
+    createTestRec( -118.8218384, 55.15506788763259, "712078 Rge Road 34" ),
+    createTestRec( -118.79719936, 55.153343057595535, "712068 Rge Road 34" ),
+    createTestRec( -118.66743097, 55.151807043809917, "712060 Rge Road 34" ),
+    createTestRec( -118.74783569, 55.155320792497442, "712082 Rge Road 35" )
+  ];
+  test.plan( expectedRecords.length * 3 + 1);
+
+  var dataStream = importPipelines.createRecordStream(
+    'test/openaddresses_sample.csv'
+  );
+  test.ok( dataStream.readable, 'Stream is readable.' );
+  var testStream = through.obj(function ( data, enc, next ){
+    var expected = expectedRecords.splice( 0, 1 )[ 0 ];
+    var centroid = data.getCentroid();
+    test.ok( expected.lon - centroid.lon < 1e-6, 'Longitude matches.' );
+    test.ok( expected.lat - centroid.lat < 1e-6, 'Latitude matches.' );
+    test.equal( expected.name, data.getName( 'default' ), 'Name matches.' );
+    next();
+  });
+  dataStream.pipe( testStream );
 });
