fe487147c44ec5f88de273c4620de6dbb4d3da40
Severyn Kozak <severyn.kozak@gmail.com>
2015-03-18 10:47:17 -0400

    Extract address validation to own function.
    lib/import_pipelines.js
    	-Extract the address-validation conditional to its own function,
    	`isValidCsvRecord()`, to allow unit-testing.
    
    test/test.js
    	-Define a unit-test for `isValidCsvRecord()`.

--- test/test.js
+++ test/test.js
@@ -118,3 +118,18 @@ tape(
     test.ok( esPipeline.writable, 'Stream is writable' );
   }
 );
+
+tape( 'Identifies invalid CSV records.', function ( test ){
+  var records = [
+    {LON: '1', LAT: '2', STREET: '3', NUMBER: '', FOO: '', SOME_PROP: ''},
+    {LON: '', LAT: '2', STREET: '3', NUMBER: '', FOO: '', SOME_PROP: 'something'},
+    {LON: '', LAT: '2', STREET: '', NUMBER: '4', SOME_PROP: 'value'}
+  ];
+  records.forEach( function ( rec ){
+    test.ok( !importPipelines.isValidCsvRecord( rec ), 'Record identified as invalid' );
+  });
+
+  var validRecord = {LON: '1', LAT: '2', STREET: '3', NUMBER: '4', SOME_PROP: 'abs'};
+  test.ok( importPipelines.isValidCsvRecord( validRecord ), 'Record identified as valid.' );
+  test.end();
+});
