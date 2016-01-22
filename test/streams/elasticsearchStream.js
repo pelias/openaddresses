347b952b4f48182e6f85e3e97811a9635dde876d
Julian Simioni <julian@simioni.org>
2016-01-22 15:00:28 -0500

    Use test.end() instead of test.plan()
    Just more commonly used in our code

--- test/streams/elasticsearchStream.js
+++ test/streams/elasticsearchStream.js
@@ -5,8 +5,8 @@ var elasticsearchStream = require( '../../lib/streams/elasticsearchStream' );
 tape(
   'importPipeline.createPeliasElasticsearchPipeline() interface',
   function ( test ){
-    test.plan( 1 );
     var esPipeline = elasticsearchStream.create();
     test.ok( esPipeline.writable, 'Stream is writable' );
+    test.end();
   }
 );
