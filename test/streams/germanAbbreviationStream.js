0101c9ce322ad34394139f939d881db1d24324e1
Alec Vulfson <alec@mapzen.com>
2016-07-29 18:00:59 -0400

    Added tests and played around with the regex

--- test/streams/germanAbbreviationStream.js
+++ test/streams/germanAbbreviationStream.js
@@ -28,6 +28,14 @@ tape( 'germanStream expands tokens ending in "-str." to "-strasse" (mostly DEU)'
         country_a: ['DEU']
       }
     },
+    {
+      address_parts: {
+        street: 'Vermeerstr'
+      },
+      parent: {
+        country_a: ['NED']
+      }
+    },
     // in Moldova the 'str.' prefix means 'Strada'
     {
       address_parts:{
@@ -65,14 +73,91 @@ tape( 'germanStream expands tokens ending in "-str." to "-strasse" (mostly DEU)'
   var stream = germanStream.create();
 
   test_stream(inputs, stream, function(err, records) {
-    test.equal(records.length, 6, 'stream length unchanged');
+    test.equal(records.length, 7, 'stream length unchanged');
 
     test.equal(records[0].address_parts.street, 'Grolmanstrasse', 'expanded');
     test.equal(records[1].address_parts.street, 'Ohrdrufer Str', 'unchanged');
-    test.equal(records[2].address_parts.street, 'str. Mircești', 'unchanged');
-    test.equal(records[3].address_parts.street, 'Sankt Nic Kirkestræde', 'expanded');
-    test.equal(records[4].address_parts.street, 'Große Str', 'unchanged');
-    test.equal(records[5].address_parts.street, 'Lindenstrasse', 'expanded');
+    test.equal(records[2].address_parts.street, 'Vermeerstraat', 'expanded');
+    test.equal(records[3].address_parts.street, 'strada Mircești', 'expanded');
+    test.equal(records[4].address_parts.street, 'Sankt Nic Kirkestræde', 'expanded');
+    test.equal(records[5].address_parts.street, 'Große Str', 'unchanged');
+    test.equal(records[6].address_parts.street, 'Lindenstrasse', 'expanded');
+    test.end();
+  });
+});
+
+  tape( 'germanStream leaves str in the middle alone', function(test) {
+  var inputs = [
+    {
+      address_parts:{
+        street: 'Grolmanstrasse'
+      },
+      parent:{
+        country_a: ['DEU']
+      }
+    },
+    {
+      address_parts:{
+        street: 'Ohrdrufer Strasse'
+      },
+      parent:{
+        country_a: ['DEU']
+      }
+    },
+    {
+      address_parts: {
+        street: 'Vermeerstraat'
+      },
+      parent: {
+        country_a: ['NED']
+      }
+    },
+    // in Moldova the 'str.' prefix means 'Strada'
+    {
+      address_parts:{
+        street: 'strada Mircești'
+      },
+      parent:{
+        country_a: ['MDA']
+      }
+    },
+    {
+      address_parts:{
+        street: 'Sankt Nic Kirkestrade'
+      },
+      parent:{
+        country_a: ['DMK']
+      }
+    },
+    {
+      address_parts:{
+        street: 'Große Strasse'
+      },
+      parent:{
+        country_a: ['DEU']
+      }
+    },
+    {
+      address_parts:{
+        street: 'Lindenstrasse'
+      } ,
+      parent:{
+        country_a: ['DEU']
+      }
+    }
+  ];
+  var stream = germanStream.create();
+
+  test_stream(inputs, stream, function(err, records) {
+    test.equal(records.length, 7, 'stream length unchanged');
+
+    test.equal(records[0].address_parts.street, 'Grolmanstrasse', 'unchanged');
+    test.equal(records[1].address_parts.street, 'Ohrdrufer Strasse', 'unchanged');
+    test.equal(records[2].address_parts.street, 'Vermeerstraat', 'unchanged');
+    test.equal(records[3].address_parts.street, 'strada Mircești', 'unchanged');
+    test.equal(records[4].address_parts.street, 'Sankt Nic Kirkestrade', 'unchanged');
+    test.equal(records[5].address_parts.street, 'Große Strasse', 'unchanged');
+    test.equal(records[6].address_parts.street, 'Lindenstrasse', 'unchanged');
     test.end();
   });
 });
