a4c67129ae9061feb8f42c3f2497e23373472d5b
Sylvain Lesage <severo@rednegra.net>
2019-09-03 12:42:05 +0200

    feat: add HTTP requests limiting on utils/ to avoid ban

--- utils/download_filtered.js
+++ utils/download_filtered.js
@@ -4,12 +4,11 @@ const async = require('async');
 const fs = require('fs-extra');
 const tmp = require('tmp');
 const logger = require('pelias-logger').get('openaddresses-download');
+const RequestQueue = require('limited-request-queue');
+const URL = require('url').URL;
 
 function downloadFiltered(config, callback) {
   const targetDir = config.imports.openaddresses.datapath;
-  const files = config.imports.openaddresses.files;
-
-  logger.info(`Attempting to download selected data files: ${files}`);
 
   fs.ensureDir(targetDir, (err) => {
     if (err) {
@@ -17,49 +16,72 @@ function downloadFiltered(config, callback) {
       return callback(err);
     }
 
-    async.eachLimit(files, 5, downloadSource.bind(null, targetDir), callback);
+    const files = getFiles(config, targetDir, callback);
+    logger.info(`Attempting to download selected data files: ${files.map(file => file.csv)}`);
 
+    // wait to avoid being banned by openaddresses.io
+    const options = {
+      maxSockets: 1,
+      rateLimit: 3000
+    };
+    const queue = new RequestQueue.default(options)
+      .on(RequestQueue.ITEM_EVENT, (url, data, done) => {
+        downloadSource(targetDir, data.file, () => done());
+      })
+      .on(RequestQueue.END_EVENT, callback);
+    files.map(file => {
+      queue.enqueue(new URL(file.url), {file: file});
+    });
   });
 
 }
 
+function getFiles(config, targetDir, main_callback){
+  const errorsFatal = config.get('imports.openaddresses.missingFilesAreFatal');
+  const files = config.imports.openaddresses.files;
+  files.forEach(file => {
+    // sources MUST end with '.csv'
+    if( !file.endsWith('.csv') ){
+      const msg = `invalid source '${file}': MUST end with '.csv'`;
+      logger.warn(msg);
+
+      // respect 'imports.openaddresses.missingFilesAreFatal' setting
+      return main_callback(errorsFatal ? msg : null);
+    }
+  });
+  return files.map(file => {
+    const source = file.replace('.csv', '.zip');
+    const name = file.replace('.csv', '').replace(/\//g,'-');
+    return {
+      csv: file,
+      url: `https://results.openaddresses.io/latest/run/${source}`,
+      zip: tmp.tmpNameSync({prefix: name, dir: targetDir, postfix: '.zip'})
+    };
+  });
+}
+
 function downloadSource(targetDir, file, main_callback) {
   const errorsFatal = config.get('imports.openaddresses.missingFilesAreFatal');
-  logger.info(`Downloading ${file}`);
-
-  const source = file.replace('.csv', '.zip');
-  const sourceUrl = `https://results.openaddresses.io/latest/run/${source}`;
-
-  // sources MUST end with '.csv'
-  if( !file.endsWith('.csv') ){
-    const msg = `invalid source '${file}': MUST end with '.csv'`;
-    logger.warn(msg);
-
-    // respect 'imports.openaddresses.missingFilesAreFatal' setting
-    return main_callback(errorsFatal ? msg : null);
-  }
-
-  const name = file.replace('.csv', '').replace(/\//g,'-');
-  const tmpZipFile = tmp.tmpNameSync({prefix: name, dir: targetDir, postfix: '.zip'});
+  logger.info(`Downloading ${file.csv}`);
 
   async.series(
     [
       // download the zip file into the temp directory
       (callback) => {
-        logger.debug(`downloading ${sourceUrl}`);
-        child_process.exec(`curl -L -X GET -o ${tmpZipFile} ${sourceUrl}`, callback);
+        logger.debug(`downloading ${file.url}`);
+        child_process.exec(`curl -L -X GET -o ${file.zip} ${file.url}`, callback);
       },
       // unzip file into target directory
       (callback) => {
-        logger.debug(`unzipping ${tmpZipFile} to ${targetDir}`);
-        child_process.exec(`unzip -o -qq -d ${targetDir} ${tmpZipFile}`, callback);
+        logger.debug(`unzipping ${file.zip} to ${targetDir}`);
+        child_process.exec(`unzip -o -qq -d ${targetDir} ${file.zip}`, callback);
       },
       // delete the temp downloaded zip file
-      fs.remove.bind(null, tmpZipFile)
+      fs.remove.bind(null, file.zip)
     ],
     function(err) {
       if (err) {
-          logger.warn(`failed to download ${sourceUrl}: ${err}`);
+          logger.warn(`failed to download ${file.url}: ${err}`);
       }
 
       // honour 'imports.openaddresses.missingFilesAreFatal' setting
