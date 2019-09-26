768ba3babd6132b60c6a6ed61a0901e3ba30b53f
missinglink <insomnia@rcpt.at>
2019-09-26 12:04:01 +0200

    feat(content-hash): improved stability for content hashes

--- test/streams/contentHashStream.js
+++ test/streams/contentHashStream.js
@@ -2,7 +2,7 @@ const tape = require('tape');
 const event_stream = require('event-stream');
 const ContentHashStream = require('../../lib/streams/contentHashStream');
 const hash = ContentHashStream.hash;
-const DEFAULT_HASH = 'd41d8cd98f00b204';
+const DEFAULT_HASH = 'ca9c491ac66b2c62';
 
 function test_stream(input, testedStream, callback) {
   var input_stream = event_stream.readArray(input);
@@ -26,7 +26,7 @@ tape('contentHashStream generates new hash', function (test) {
     test.equal(records.length, 1, 'stream length unchanged');
 
     var record = records[0];
-    test.equal(record.HASH, 'c2f8c35aa279ee7d', 'HASH field generated');
+    test.equal(record.HASH, 'f44048507e8fb319', 'HASH field generated');
     test.end();
   });
 });
@@ -47,71 +47,121 @@ tape('contentHashStream replaces existing hash', function (test) {
     test.equal(records.length, 1, 'stream length unchanged');
 
     var record = records[0];
-    test.equal(record.HASH, 'c2f8c35aa279ee7d', 'HASH field generated');
+    test.equal(record.HASH, 'f44048507e8fb319', 'HASH field generated');
     test.end();
   });
 });
 
 tape('hash: default value for non-object and empty objects', function (test) {
-  test.equal(hash(null), DEFAULT_HASH);
-  test.equal(hash(1), DEFAULT_HASH);
-  test.equal(hash(false), DEFAULT_HASH);
-  test.equal(hash('string'), DEFAULT_HASH);
-  test.equal(hash([]), DEFAULT_HASH);
-  test.equal(hash({}), DEFAULT_HASH);
+  test.equal(hash(null), DEFAULT_HASH, 'default hash');
+  test.equal(hash(1), DEFAULT_HASH, 'default hash');
+  test.equal(hash(false), DEFAULT_HASH, 'default hash');
+  test.equal(hash('string'), DEFAULT_HASH, 'default hash');
+  test.equal(hash([]), DEFAULT_HASH, 'default hash');
+  test.equal(hash({}), DEFAULT_HASH, 'default hash');
   test.end();
 });
 
-tape('hash: 16 hexidecimal chars', function (test) {
-  const h = hash({ field: 'value' });
-  test.true(/[0-9A-Fa-f]{16}/g.test(h));
+tape('hash: 16 lowercase hexidecimal chars', function (test) {
+  const conform = /^[0-9a-f]{16}$/;
+  for( let i=-90.0; i<+90.0; i+=0.5 ){
+    let h = hash({ LON: i, LAT: i });
+    test.true(conform.test(h), h);
+  }
   test.end();
 });
 
 tape('hash: strict equality', function (test) {
   test.equal(
-    hash({ field1: 'A', field2: 'B' }),
-    hash({ field1: 'A', field2: 'B' })
+    hash({ LON: '1.1', LAT: '2.2' }),
+    hash({ LON: '1.1', LAT: '2.2' })
+  );
+  test.equal(
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST' }),
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST' })
+  );
+  test.equal(
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST', NUMBER: '10' }),
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST', NUMBER: '10' })
+  );
+  test.equal(
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST', NUMBER: '10', UNIT: '6B' }),
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST', NUMBER: '10', UNIT: '6B' })
   );
   test.end();
 });
 
 tape('hash: ingore existing hash field', function (test) {
   test.equal(
-    hash({ field1: 'A', field2: 'B', hash: 'c2f8c35aa279ee7d' }),
-    hash({ field1: 'A', field2: 'B', HASH: 'deadb33fdeadb33f' })
+    hash({ LON: '1.1', LAT: '2.2', HASH: 'c2f8c35aa279ee7d' }),
+    hash({ LON: '1.1', LAT: '2.2', HASH: 'deadb33fdeadb33f' })
   );
   test.end();
 });
 
 tape('hash: fuzzy equality', function (test) {
   test.equal(
-    hash({ field1: 'A' }),
-    hash({ FIELD1: 'A' }),
-    'key case'
-  );
-  test.equal(
-    hash({ field1: 'A' }),
-    hash({ field1: 'a' }),
+    hash({ STREET: 'A ST' }),
+    hash({ STREET: 'a st' }),
     'value case'
   );
   test.equal(
-    hash({ field1: 'A' }),
-    hash({ field1: ' A ' }),
+    hash({ STREET: 'A ST' }),
+    hash({ STREET: ' A  ST  ' }),
     'value whitespace'
   );
   test.equal(
-    hash({ field1: 1 }),
-    hash({ field1: '1' }),
+    hash({ STREET: 1 }),
+    hash({ STREET: '1' }),
     'value type'
+  );
+  test.equal(
+    hash({ LON: 1.123456789 }),
+    hash({ LON: 1.1234567 }),
+    'float precision'
+  );
+  test.equal(
+    hash({ LON: 1.12000000000 }),
+    hash({ LON: 1.12 }),
+    'float precision'
+  );
+  test.equal(
+    hash({ LON: -1.000000000000 }),
+    hash({ LON: -1 }),
+    'float precision'
+  );
+  test.equal(
+    hash({ LON: 0 }),
+    hash({ LON: -0 }),
+    'negative zero'
   );
   test.end();
 });
 
 tape('hash: strict inequality', function (test) {
   test.notEqual(
-    hash({ field1: 'A', field2: 'B' }),
-    hash({ field1: 'A', field9: 'B' })
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST', NUMBER: '10', UNIT: '6B' }),
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST', NUMBER: '10', UNIT: '6' })
+  );
+  test.notEqual(
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST', NUMBER: '10' }),
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST', NUMBER: '11' })
+  );
+  test.notEqual(
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A ST' }),
+    hash({ LON: '1.1', LAT: '2.2', STREET: 'A RD' })
+  );
+  test.notEqual(
+    hash({ LON: '1.1', LAT: '2.2' }),
+    hash({ LON: '1.1', LAT: '2.1' })
+  );
+  test.notEqual(
+    hash({ LON: '1.1' }),
+    hash({ LON: '-1.1' })
+  );
+  test.notEqual(
+    hash({ NUMBER: '10' }),
+    hash({ UNIT: '10' })
   );
   test.end();
 });
