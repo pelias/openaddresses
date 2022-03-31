5a9934842672f0b9bda888077d6ea600b6eaceb0
Peter Johnson <insomnia@rcpt.at>
2022-03-31 22:56:17 +0200

    fix(gnaf): silence gnaf mapper warning when ID field is undefined (#505)

--- test/streams/gnafMapperStream.js
+++ test/streams/gnafMapperStream.js
@@ -43,11 +43,32 @@ module.exports.tests.au_gnaf_id = function (test) {
   });
 };
 
-module.exports.tests.non_au_gnaf_id = function (test) {
+module.exports.tests.au_invalid_gnaf_id = function (test) {
   var doc = new Document('oa', 'a', 1);
   doc.setMeta('country_code', 'AU');
   doc.setMeta('oa', {
     ID: 'invalid', // note: invalid GNAF ID
+    NUMBER: '360',
+    STREET: 'BRUNSWICK STREET',
+    LAT: -37.79647546,
+    LON: 144.978997
+  });
+  test('maps - GNAF ID', t => {
+    var stream = mapper();
+    stream.pipe(through.obj((doc, enc, next) => {
+      t.deepEqual(doc.getAddendum('concordances'), undefined);
+      t.end();
+      next();
+    }));
+    stream.write(doc);
+  });
+};
+
+module.exports.tests.au_missing_id_field = function (test) {
+  var doc = new Document('oa', 'a', 1);
+  doc.setMeta('country_code', 'AU');
+  doc.setMeta('oa', {
+    ID: undefined, // note: missing ID field
     NUMBER: '360',
     STREET: 'BRUNSWICK STREET',
     LAT: -37.79647546,
